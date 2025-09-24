e3fd59677557a5dc3ecce7a321c7b61b30d429c9
tabaken8 <ken314ta@gmail.com>
2025-09-24 21:26:35 +0900

    chore: ignore ESLint errors during Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // 本番ビルドでESLintエラーがあっても落とさない
+    ignoreDuringBuilds: true,
+  },
+  // TypeScript型エラーでも落ちる場合は、必要なときだけ下をON
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+}
+module.exports = nextConfig
